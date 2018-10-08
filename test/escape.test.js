d4855decfc989c5f6c4c091849e73d63382af227
Denis Fäcke <serayaeryn@gmail.com>
2018-10-08 18:42:19 +0200

    allow square brackets in escaped

--- test/escape.test.js
+++ test/escape.test.js
@@ -11,3 +11,13 @@ test('should respect escaped chars', (t) => {
 
   t.strictEquals(formatted, 'GMT')
 })
+
+test('should allows square brackets in escaped chars', (t) => {
+  t.plan(1)
+  var date = new Date(2000, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('[[GMT]')
+
+  var formatted = dateFormatter.format(date)
+
+  t.strictEquals(formatted, '[GMT')
+})
