d4f9832b3c5aa721ffbab7b6bad29be7e0f1832c
Denis Fäcke <serayaeryn@gmail.com>
2025-06-15 22:04:50 +0200

    use private class fields

--- test/year.test.js
+++ test/year.test.js
@@ -10,11 +10,65 @@ test('YYYY', (t) => {
   t.is(formatted, '2000')
 })
 
-test('YY', (t) => {
+test('YY - 2000', (t) => {
   var date = new Date(2000, 2, 1, 3, 4, 5, 1)
   var dateFormatter = new DateFormatter('YY')
 
   var formatted = dateFormatter.format(date)
 
   t.is(formatted, '00')
+})
+
+test('YY - 2001', (t) => {
+  var date = new Date(2001, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('YY')
+
+  var formatted = dateFormatter.format(date)
+
+  t.is(formatted, '01')
+})
+
+test('YY - 2011', (t) => {
+  var date = new Date(2011, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('YY')
+
+  var formatted = dateFormatter.format(date)
+
+  t.is(formatted, '11')
+})
+
+test('YY - 1900', (t) => {
+  var date = new Date(1900, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('YY')
+
+  var formatted = dateFormatter.format(date)
+
+  t.is(formatted, '00')
+})
+
+test('YY - 1901', (t) => {
+  var date = new Date(1901, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('YY')
+
+  var formatted = dateFormatter.format(date)
+
+  t.is(formatted, '01')
+})
+
+test('YY - 1911', (t) => {
+  var date = new Date(1911, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('YY')
+
+  var formatted = dateFormatter.format(date)
+
+  t.is(formatted, '11')
+})
+
+test('YY - 1811', (t) => {
+  var date = new Date(1811, 2, 1, 3, 4, 5, 1)
+  var dateFormatter = new DateFormatter('YY')
+
+  var formatted = dateFormatter.format(date)
+
+  t.is(formatted, '11')
 })
